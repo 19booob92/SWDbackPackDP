2aa44157f461b283f3052ffa829bbbb2955863ca
19booob92 <19mateusz92@gmail.com>
2015-05-20 23:55:45 +0200

    implemented algorithm

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,11 +1,16 @@
 package main;
 
+import main.core.dto.Parameters;
+import main.dynamicProgramming.BackpackProblemSolver;
+
 
 public class Runner {
 
-    
-    
     public static void main(String[] args) {
+        Parameters params = new Parameters();
         
+        BackpackProblemSolver solver = new BackpackProblemSolver(params);
+        
+        System.out.println(solver.backTracking());
     }
 }
--- src/main/dynamicProgramming/BackpackProblemSolver.java
+++ src/main/dynamicProgramming/BackpackProblemSolver.java
@@ -1,19 +1,49 @@
 package main.dynamicProgramming;
 
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
+import main.core.dto.Parameters;
 
 public class BackpackProblemSolver {
 
-    private List<Integer> matrix = new ArrayList<>(0);
-    
-    public BackpackProblemSolver() {
-        
+    private int[][] matrix;
+
+    private Parameters parameters;
+
+    public BackpackProblemSolver(Parameters parameters) {
+        this.parameters = parameters;
+        matrix = new int[parameters.getItemsAmount()][parameters.getPackSize()];
+    }
+
+    private void solveProblem() {
+        for (int itemIdx = 1; itemIdx < parameters.getItemsAmount() + 1; itemIdx++) {
+            for (int b = 1; b < parameters.getPackSize() + 1; b++) {
+                if (b >= parameters.getSizes().get(itemIdx - 1)) {
+                    matrix[itemIdx][b] = Math.max(matrix[itemIdx - 1][b - parameters.getSizes().get(itemIdx - 1)] + parameters.getPrices().get(itemIdx - 1), matrix[itemIdx - 1][b]);
+                }
+            }
+        }
     }
     
-    public void solveProblem() {
+    public List<Boolean> backTracking() {
+        solveProblem();
         
+        int b = parameters.getPackSize();
+        List<Boolean> vector = new ArrayList<>();
+        
+        for (int i = parameters.getItemsAmount(); i >= 0; i--) {
+            if (matrix[i][b] == matrix[i-1][b]) {
+                vector.add(false);
+            } else {
+                vector.add(true);
+                b -= parameters.getSizes().get(i-1);
+            }
+        }
+        Collections.reverse(vector);
+        
+        return vector;
     }
     
 }
