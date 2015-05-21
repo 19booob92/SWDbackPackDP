564911eead8d69d7377b23bdcd4308b911627f3d
19booob92 <19mateusz92@gmail.com>
2015-05-21 08:40:37 +0200

    working algorithm
    bug in formulas, should be refactored

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,16 +1,29 @@
 package main;
 
+import java.util.List;
+import javax.swing.JOptionPane;
 import main.core.dto.Parameters;
 import main.dynamicProgramming.BackpackProblemSolver;
-
+import main.utils.FileLoader;
+import main.utils.InputDataProcessor;
 
 public class Runner {
 
-    public static void main(String[] args) {
-        Parameters params = new Parameters();
+    public static void main(String[] args) throws Exception {
+        InputDataProcessor dataProcessor = new InputDataProcessor();
+        Parameters params;
+
+        try {
+            List<String> data = FileLoader.readFile("/home/booob/Documents/workspace-sts-3.5.1.RELEASE/SWDProblemPlecakowy/resources/wagi.txt");
+            params = dataProcessor.processData(data);
+          
+            BackpackProblemSolver solver = new BackpackProblemSolver(params);
+
+            System.out.println(solver.backTracking());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Nie odnaleziono pliku");
+            System.err.println(ex);
+        }
         
-        BackpackProblemSolver solver = new BackpackProblemSolver(params);
-        
-        System.out.println(solver.backTracking());
     }
 }
--- src/main/dynamicProgramming/BackpackProblemSolver.java
+++ src/main/dynamicProgramming/BackpackProblemSolver.java
@@ -1,6 +1,5 @@
 package main.dynamicProgramming;
 
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,36 +13,46 @@ public class BackpackProblemSolver {
 
     public BackpackProblemSolver(Parameters parameters) {
         this.parameters = parameters;
-        matrix = new int[parameters.getItemsAmount()][parameters.getPackSize()];
+        matrix = new int[parameters.getItemsAmount() + 1][parameters.getPackSize() + 1];
     }
 
     private void solveProblem() {
-        for (int itemIdx = 1; itemIdx < parameters.getItemsAmount() + 1; itemIdx++) {
-            for (int b = 1; b < parameters.getPackSize() + 1; b++) {
+        for (int itemIdx = 1; itemIdx <= parameters.getItemsAmount(); itemIdx++) {
+            for (int b = 1; b <= parameters.getPackSize(); b++) {
                 if (b >= parameters.getSizes().get(itemIdx - 1)) {
                     matrix[itemIdx][b] = Math.max(matrix[itemIdx - 1][b - parameters.getSizes().get(itemIdx - 1)] + parameters.getPrices().get(itemIdx - 1), matrix[itemIdx - 1][b]);
                 }
             }
         }
     }
-    
+
     public List<Boolean> backTracking() {
         solveProblem();
-        
+
         int b = parameters.getPackSize();
         List<Boolean> vector = new ArrayList<>();
-        
-        for (int i = parameters.getItemsAmount(); i >= 0; i--) {
-            if (matrix[i][b] == matrix[i-1][b]) {
+
+        for (int i = parameters.getItemsAmount() - 1; i > 0; i--) {
+            if (matrix[i][b] == matrix[i - 1][b]) {
                 vector.add(false);
             } else {
                 vector.add(true);
-                b -= parameters.getSizes().get(i-1);
+                b -= parameters.getSizes().get(i - 1);
             }
         }
         Collections.reverse(vector);
+
+        printSteps();
         
         return vector;
     }
-    
+
+    private void printSteps() {
+        for (int itemIdx = 0; itemIdx <= parameters.getItemsAmount(); itemIdx++) {
+            for (int b = 0; b <= parameters.getPackSize(); b++) {
+                System.out.printf("%4d", matrix[itemIdx][b]);
+            }
+           System.out.println();
+        }
+    }
 }
